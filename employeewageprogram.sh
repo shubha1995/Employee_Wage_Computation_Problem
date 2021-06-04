a5678eb96ec10ca737fb8c58d3238edd489e31f6
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-04 13:10:36 +0530

    Added:Solving_Using_Case_Statement

--- employeewageprogram.sh
+++ employeewageprogram.sh
@@ -5,14 +5,33 @@ randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
-	function partTimeEmployeeAndWage(){
-	read -p "Assume Wage per hour is :" isWagePerHour
-	read -p "To part time hour is :" isFullDayHour
-	calculatePartTimeEmployeeWage=$((isWagePerHour*isFullDayHour))
-	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage
-}
-partTimeEmployeeAndWage
+	echo "Enter choice: "
+	echo "1.Full Time Employee"
+	echo "2.Part Time Employee"
+	read ch
+case "$ch" in
+	1) echo "To calculate Daily Employee Wage"
+		function calculateDailyEmployeeWae(){
+		read -p "Assume Wage per hour is :" isWagePerHour
+		read -p "To full day hours is :" isFullDayHour
+		calculateEmployeeWage=$((isWagePerHour*isFullDayHour))
+		echo "To calculate daily employee wage :" $calculateEmployeeWage
+		}
+		calculateDailyEmployeeWae
+		;;
+	2)echo "To calculate part time Employee and wage"
+		function partTimeEmployeeAndWage(){
+		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+		read -p "To part time hour is :" isFullDayHour
+		calculatePartTimeEmployeeWage=$((isWagePerHour*isFullDayHour))
+		echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage
+			}
+		partTimeEmployeeAndWage
+		;;
+		*)echo "No choice"
+		;;
+esac
+
 else
    echo "Employee Absent"
  fi
-
