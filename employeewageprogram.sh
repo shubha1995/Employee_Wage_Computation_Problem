8923edd42f7a91fac5456bbb12d38e88eaf4fca6
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-04 12:59:57 +0530

    Added:Calculate Daily emp wage

--- employeewageprogram.sh
+++ employeewageprogram.sh
@@ -1,10 +1,19 @@
 echo $"Welcome to EmployeeWage Computation Program"
 
 isPresent=1
+wph=20
+fdh=8
 randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
+function calculateDailyEmployeeWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour
+	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$((isWagePerHour *isFullDayHour))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage
+}
+calculateDailyEmployeeWage
 else
-	echo "Employee Absent"
+        echo "Employee Absent"
 fi
