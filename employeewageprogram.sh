ab7d3742693f4c46aad278926b0fd2165fad2f6f
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-04 13:05:21 +0530

    Added:Add_Part_Time_Employee_And_Wage

--- employeewageprogram.sh
+++ employeewageprogram.sh
@@ -1,19 +1,18 @@
 echo $"Welcome to EmployeeWage Computation Program"
 
 isPresent=1
-wph=20
-fdh=8
 randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
-function calculateDailyEmployeeWage(){
+	function partTimeEmployeeAndWage(){
 	read -p "Assume Wage per hour is :" isWagePerHour
-	read -p "To full day hours is :" isFullDayHour
-	calculateEmployeeWage=$((isWagePerHour *isFullDayHour))
-	echo "To calculate daily employee wage :" $calculateEmployeeWage
+	read -p "To part time hour is :" isFullDayHour
+	calculatePartTimeEmployeeWage=$((isWagePerHour*isFullDayHour))
+	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage
 }
-calculateDailyEmployeeWage
+partTimeEmployeeAndWage
 else
-        echo "Employee Absent"
-fi
+   echo "Employee Absent"
+ fi
+
